1f47b84787b87039c88f5900db3bb6ffafeb2168
pcsailor <Phil@philcurtis.io>
2018-11-23 08:17:23 -0600

    working on db models, pg42

--- app/__init__.py
+++ app/__init__.py
@@ -3,14 +3,14 @@
 * from 'file' import 'class'
 '''
 from flask import Flask #1
-# from flask_sqlalchemy import SQLAlchemy #
-# from flask_migrate import Migrate #
 from config import Config
+from flask_sqlalchemy import SQLAlchemy #
+from flask_migrate import Migrate #
 
 
 app = Flask(__name__) #2 an instance of class Flask within the __init__.py script
 app.config.from_object(Config) # reads & applies the config.py file
-#db = SQLAlchemy(app) #
-# migrate = Migrate(app, db) #
+db = SQLAlchemy(app) #
+migrate = Migrate(app, db) #
 
-from app import routes# , models #3 'app' refers to folder # routes always-at-bottom due to 'circular imports'
+from app import routes, models #3 'app' refers to folder # routes always-at-bottom due to 'circular imports'
