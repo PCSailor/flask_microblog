985c97c45ac494a983732fe3c042e5df303210e8
pcsailor <phil@philcurtis.io>
2018-11-24 03:10:39 -0600

    complete through Requiring Users To Login, pg66

--- app/__init__.py
+++ app/__init__.py
@@ -1,26 +1,20 @@
 '''
 * #1 startup file
 * from 'file' import 'class'
-
-Error = "cannot import name 'db' from 'app'"
-you are importing main BEFORE you are creating the instance of db in your __init__.py
-
-If move the import to after your db = SQLAlchemy(app), it will work
-
-db = SQLAlchemy(app)
-
-from bookshelf.main.controllers import main #<--move this here
-
 '''
 from flask import Flask #1
 from config import Config
 from flask_sqlalchemy import SQLAlchemy #
 from flask_migrate import Migrate #
+from flask_login import LoginManager # 
+
 
 
 app = Flask(__name__) #2 an instance of class Flask within the __init__.py script
 app.config.from_object(Config) # reads & applies the config.py file
 db = SQLAlchemy(app) #
 migrate = Migrate(app, db) #
+login = LoginManager(app)
+login.login_view = 'login'
 
 from app import routes, models #3 'app' refers to folder # routes always-at-bottom due to 'circular imports'
--- app/routes.py
+++ app/routes.py
@@ -2,12 +2,17 @@
 * #2 startup file
 
 '''
-from flask import render_template, flash, redirect, url_for #
+from flask import render_template, flash, redirect, url_for, request #
+from flask_login import current_user, login_user, logout_user, login_required
 from app import app #1
 from app.forms import LoginForm # import the Login class from forms.py, instantiated an object from it, & sent it to the template
+from app.models import User
+from werkzeug.urls import url_parse
+
 # Decorators modifiy the function that follows it
 @app.route('/') #2 # decorator
 @app.route('/index') #3 # decorator
+@login_required
 
 def index(): #4
     # return "routes.py is hea!!" #5
@@ -25,10 +30,25 @@ def index(): #4
     return render_template('index.html', title='Home', user=user, posts=posts)
 
 @app.route('/login', methods=['GET', 'POST']) # decorator # BUG: if kept above login screen fails
-
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit(): # method doing all from processing
-        flash('Login requested for user {}, remember_me={}'.format(form.username.data,form.remember_me.data))
-        return redirect(url_for('index')) # return redirect('/index')
+        user = User.query.filter_by(username=form.userform.data).first() # query db for entered user name
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid user name or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page) # return redirect('/index')
+        # return redirect(url_for('index')) # return redirect('/index')
     return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
