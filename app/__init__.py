85aa949a7bedbbca4bf4a2157fab0f21b04b8801
pcsailor <phil@philcurtis.io>
2018-11-26 22:54:53 -0600

    Duplicate Username Bug code added successfully

--- app/__init__.py
+++ app/__init__.py
@@ -10,7 +10,7 @@ from config import Config
 from flask_migrate import Migrate #
 from flask_sqlalchemy import SQLAlchemy #
 from flask_login import LoginManager # 
-from flask_mail import Mail
+# from flask_mail import Mail
 
 
 app = Flask(__name__) #2 an instance of class Flask within the __init__.py script
@@ -19,16 +19,16 @@ db = SQLAlchemy(app) #
 migrate = Migrate(app, db) #
 login = LoginManager(app)
 login.login_view = 'login'
-mail = Mail(app)
+# mail = Mail(app)
 
 if not app.debug:
     if app.config['MAIL_SERVER']:
         auth = None
         if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
-            auth = app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD']
+            auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD']) # error here=forgot two ()
         secure = None
         if app.config['MAIL_USE_TLS']:
-            secure()
+            secure = ()
         mail_handler = SMTPHandler(
             mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
             fromaddr='no-reply@' + app.config['MAIL_SERVER'],
