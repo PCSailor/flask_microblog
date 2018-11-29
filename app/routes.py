8a1b57b6d64198a2c74fed4325f25843cee9a0fb
pcsailor <phil@philcurtis.io>
2018-11-29 03:55:19 -0600

    complete through ch08 but still bugs with SMTPServer & tests.py

--- app/routes.py
+++ app/routes.py
@@ -2,28 +2,29 @@
 * #2 startup file
 
 '''
-from flask import render_template, flash, redirect, url_for, request #
+from flask import render_template, flash, redirect, url_for, request
 from flask_login import current_user, login_user, logout_user, login_required
-from app import app #1
+from app import app
 from app import db
-from app.forms import LoginForm, RegistrationForm, EditProfileForm # import the Login class from forms.py, instantiated an object from it, & sent it to the template
+from app.forms import LoginForm, RegistrationForm, EditProfileForm
+# import Login class, instantiated an object, & sent to template
 from app.models import User
 from werkzeug.urls import url_parse
 from datetime import datetime
 
 # Decorators modifiy the function that follows it
-@app.route('/') #2 # decorator
-@app.route('/index') #3 # decorator
+@app.route('/')  # decorator
+@app.route('/index')  # decorator
 @login_required
-def index(): #4
+def index():
     # return "routes.py is hea!!" #5
     # user = {'username': 'Phil'}
-    posts = [ # a list
-        { # dictionary
+    posts = [  # a list
+        {  # dictionary
             'author': {'username': 'Phil'},
             'body': 'Post #1 comng from routes.py'
         },
-        { # another dictionary
+        {  # another dictionary
             'author': {'username': 'Steph'},
             'body': 'Post #2 comng from routes.py.'
         }
@@ -36,8 +37,8 @@ def login():
     if current_user.is_authenticated:
         return redirect(url_for('index'))
     form = LoginForm()
-    if form.validate_on_submit(): # method doing all from processing
-        user = User.query.filter_by(username=form.username.data).first() # query db for entered user name
+    if form.validate_on_submit():  # method doing all from processing
+        user = User.query.filter_by(username=form.username.data).first()  # query db for entered user name
         if user is None or not user.check_password(form.password.data):
             flash('Invalid user name or password')
             return redirect(url_for('login'))
@@ -45,7 +46,7 @@ def login():
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
             next_page = url_for('index')
-        return redirect(next_page) # return redirect('/index')
+        return redirect(next_page)  # return redirect('/index')
         # return redirect(url_for('index')) # return redirect('/index')
     return render_template('login.html', title='Sign In', form=form)
 
@@ -62,7 +63,7 @@ def register():
     Logic done inside 'if validate_on_submit()' conditional creates new user with username-email-password, writes it to database, & redirects to login prompt so user can log in
     '''
     if current_user.is_authenticated:
-        return redirect(url_for('index')) # ensure the user is not logged in
+        return redirect(url_for('index'))  # ensure the user is not logged in
     form = RegistrationForm()
     if form.validate_on_submit():
         user = User(username=form.username.data, email=form.email.data)
@@ -85,8 +86,8 @@ def user(username):
     return render_template('user.html', user=user, posts=posts)
 
 # Last Visit Time
-@app.before_request # flask register decorator implementation checks if the current_user is logged in, & if so sets the last_seen field to the current time
-def before_request(): # view function
+@app.before_request  # flask register decorator implementation checks if the current_user is logged in, & if so sets the last_seen field to the current time
+def before_request():  # view function
     if current_user.is_authenticated:
         current_user.last_seen = datetime.utcnow()
         db.session.commit()
@@ -95,7 +96,7 @@ def before_request(): # view function
 @app.route('/edit_profile', methods=['GET', 'POST'])
 @login_required
 def edit_profile():
-    form = EditProfileForm(current_user.username) # Fix duplicate username bug
+    form = EditProfileForm(current_user.username)  # Fix duplicate username bug
     if form.validate_on_submit():
         current_user.username = form.username.data
         current_user.about_me = form.about_me.data
@@ -107,4 +108,31 @@ def edit_profile():
         form.about_me.data = current_user.about_me
     return render_template('edit_profile.html', title='Edit Profile', form=form)
 
-    # form = EditProfileForm(current_user.username) # Fix duplicate username bug
+    # follow user
+    @app.route('/follow/<username>')
+    @login_required
+    def follow(username):
+        user = User.query.filter_by(username=username).first()
+        if user is None:
+            flash('user {} not found.'.format(username))
+            return redirect(url_for('index'))
+        if user == current_user:
+            flash('Do you really want to follow yourself?')
+            return redirect(url_for('user', username=username))
+        current_user.follow(user)
+        db.session.commit()
+        flash('You are now following {} <:'.format(username))
+
+    # unfollow user
+    def unfollow(username):
+        user = User.query.filter_by(username=username).first()
+        if user is None:
+            flash('User {} not found.'.format(username))
+            return redirect(url_for('index'))
+        if user == current_user:
+            flash('You cannot unfollow yourself!')
+            return redirect(url_for('user', username=username))
+        current_user.unfollow(user)
+        db.session.commit()
+        flash('You are no longer following {}.'.format(username))
+        return redirect(url_for('user', username=username))
